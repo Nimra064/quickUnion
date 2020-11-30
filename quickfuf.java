b4bafa7225645977fd8bd13f31a76000e2aea406
Nimra <nimrashahzadisa064@gmail.com>
2020-11-29 20:29:49 -0800

    QuickUnionalgo

---
+++ quickfuf.java
@@ -0,0 +1,53 @@
+public class quickfuf{
+
+	private  int []  parent;
+
+
+	public  void quickfuf(int n)
+	{
+		parent=new int[n];
+		for(int i=0; i<n; i++)
+			parent[i]=i;
+	}
+
+	public  int find(int p)
+	{
+		while(p != parent[p])
+			 p = parent[p];
+        return p;
+    }
+
+    
+    private void validate(int p) {
+        int n = parent.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n-1));
+        }
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+        parent[rootP] = rootQ; 
+       
+    }
+     public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+	
+
+
+	public  static  void main(String[] args)
+	{
+		 int n = StdIn.readInt();
+		QuickUnionUF uf = new QuickUnionUF(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.find(p) == uf.find(q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+	}
+}
